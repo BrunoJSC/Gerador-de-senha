64107fcf5fbde1950319332f0c4454a5095d9100
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2020-07-01 20:37:08 -0500

    Exemplo

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const pass = document.querySelector('#pass');
+const newPassword = document.querySelector('#newPassword');
+
+function generatePass() {
+  if(pass.value === '') {
+    alert("Digite o tamanho da sua senha!",);
+    return;
+  }
+  let charset = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUWXYZ123456789";
+  let password = "";
+
+  for(var i = 0, n = charset.length; i< pass.value; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  let result = document.createTextNode(password);
+  return newPassword.appendChild(result);
+};
